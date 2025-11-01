171bb979369cb6451029078f753ef79eb6e78c05
younghwan <younghwan.ji@gmail.com>
2025-11-01 13:04:55 +0900

    [Init] Init project
    [Details]
    Init springboot project.
    - SpringBoot 3.5.7
    - Java 21
    
    - Dependency
      - Spring Web
    
    [Issue]
    N/A

---
+++ src/main/java/com/template/tspringbootjava/TSpringbootJavaApplication.java
@@ -0,0 +1,13 @@
+package com.template.tspringbootjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TSpringbootJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TSpringbootJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/template/tspringbootjava/TSpringbootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.template.tspringbootjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TSpringbootJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
